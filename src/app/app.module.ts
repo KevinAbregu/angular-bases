e6a6966ab1a7162212799f4711f267c969247b8d
Kevin Abregu <kmabregu4@gmail.com>
2024-01-27 22:29:04 +0100

    Added components heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
