774fd11362e1155ca30c82014acce813338feb02
Kevin Abregu <kmabregu4@gmail.com>
2024-01-28 23:50:15 +0100

    delete element from list

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -7,9 +7,18 @@ import { Character } from '../../interfaces/character.interface';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  @Output()
+  public onDeleteId: EventEmitter<number> = new EventEmitter();
+
   @Input()
   public characterList: Character[] =  [{
     name: 'Trunk',
     power: 10
   }]
+
+  onDeleteCharacter(index: number): void {
+    this.onDeleteId.emit(index)
+
+  }
 }
