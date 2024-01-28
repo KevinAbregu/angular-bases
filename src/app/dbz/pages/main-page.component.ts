a0fd84cd289e6051193d27d2fed2d6ad1d940188
Kevin Abregu <kmabregu4@gmail.com>
2024-01-29 00:00:19 +0100

    First service

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -8,25 +8,7 @@ import { Character } from '../interfaces/character.interface';
 
 export class MainPageComponent  {
 
-    public characters: Character[] = [
-        {
-            name: 'Krillin',
-            power: 1000,
-        },
-        {
-            name: 'Goku',
-            power: 9500
-        },{
-            name: 'Vegetta',
-            power: 7500
-        }
-    ]
-
-    onNewCharacter(character: Character):void {
-        this.characters.push(character);
-    }
-
-    onDeleteCharacter(index: number):void {
-        this.characters.splice(index, 1)
+    constructor( public dbzService: DbzService ) {
+        
     }
 }
